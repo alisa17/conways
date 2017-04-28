316884f03edbdecc874ef392ca3f59b4ec33de30
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-04-28 13:14:26 +1200

    alter birthCell

--- tests/tests.js
+++ tests/tests.js
@@ -8,26 +8,26 @@ test('test setup working', function(t) {
 
 
 test('able to create an empty board of specified size', function(t) {
-var size = 5
-var expected = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
-var actual = game.createBoard(size)
-t.deepEqual(actual, expected)
-t.end()
+    var size = 5
+    var expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+    var actual = game.createBoard(size)
+    t.deepEqual(actual, expected)
+    t.end()
 })
 
 test('create a thing that is true a specified percentage of times', function(t) {
-  var percentage = 0.4
-  var expectedMaximum = 50
-  var actual = 0
-  for (var i = 0; i < 100; i++) {
-  if(game.birthCell(percentage) == true) actual++
- }
-  t.true(actual < expectedMaximum)
-  t.end()
+    var percentage = 0.4
+    var expectedMaximum = 50
+    var actual = 0
+    for (var i = 0; i < 100; i++) {
+        actual+=game.birthCell(percentage)
+    }
+    t.true(actual < expectedMaximum)
+    t.end()
 })
